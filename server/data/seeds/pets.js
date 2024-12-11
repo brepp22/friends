80c91c169ede2b29e9c44ec008888f0b830653ee
brepp22 <brepp@hotmail.com>
2024-12-10 17:43:15 -0800

    seeds updated

--- server/data/seeds/pets.js
+++ server/data/seeds/pets.js
@@ -9,10 +9,10 @@ exports.seed = async function(knex) {
       petname: "Fluffy" , breed: "Pitbull" , weight: 57, color: "Brown", 
       bio: "Fluffy is a sweet girl with a spunky personality. Loves to go on hikes and long walks on the beach."},
     {pet_id: 2, img: "https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      petname: "Dash" , breed: "Tabby" , weight: 8, color: "Silver", 
+      petname: "Dash" , breed: "Maine Coon" , weight: 8, color: "Silver", 
       bio: "Dash is a curious and cuddly companion. Loves to hang out with the family."},
     {pet_id: 3, img: "https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      petname: "Boulder" , breed: "French Bulldog" , weight: 27, color: "Cream", 
+      petname: "Boulder" , breed: "French Bulldog" , weight: 24, color: "Cream", 
       bio: "Boulder is a free spirit with a lot of charm. Loves to cuddle and go on car rides."}
   ]);
 };
