12d6862ad0aa5874e67213ffa5bbdf6b34a8f91c
brepp22 <brepp@hotmail.com>
2024-12-22 15:30:25 -0800

    landing wip

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -7,14 +7,9 @@ export default function Landing() {
     
     return (
         <div className = 'landing'>
-             <div className = 'landing-img'>
-            <img  src='pawfectlogo.png' alt = 'french bulldog'/>
+            <div className = 'landing-img'>
+            <img  src='petconnectlanding.png' alt = 'french bulldog'/>
             </div> 
-            <div className = 'landing-info'>
-                <h1> Welcome to Elleanor's Pawfect Matches</h1>
-                <h2> Login to see your pawfect match</h2>
-            </div>
-          
         </div>
     )
 } 
