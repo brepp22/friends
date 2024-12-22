10d9e55f95678db1b6253f32a0ce76fc041e5d82
brepp22 <brepp@hotmail.com>
2024-12-22 15:58:12 -0800

    landing wip

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -6,10 +6,27 @@ export default function Landing() {
 
     
     return (
-        <div className = 'landing'>
-            <div className = 'landing-img'>
-            <img  src='petconnectlanding.png' alt = 'french bulldog'/>
-            </div> 
+        <div className="landing">
+          {/* Full-width image */}
+          <div className="landing-banner">
+            <img src="petconnectlanding.png" alt="French Bulldog" />
+          </div>
+      
+          {/* Boxed images section */}
+          <div className="function-landing">
+            <div className="image-box">
+              <img src="Login.png" alt="Login" />
+            </div>
+            <div className="image-box">
+              <img src="Login.png" alt="Login" />
+            </div>
+            <div className="image-box">
+              <img src="Login.png" alt="Login" />
+            </div>
+            <div className="image-box">
+              <img src="Login.png" alt="Login" />
+            </div>
+          </div>
         </div>
-    )
-} 
+      );
+    }
