4f5811b60ba9d9a0cc7d54499cf76749b96d31f6
brepp22 <brepp@hotmail.com>
2024-12-22 16:33:14 -0800

    landing wip

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 
 export default function Landing() {
@@ -7,26 +8,18 @@ export default function Landing() {
     
     return (
         <div className="landing">
-          {/* Full-width image */}
+            
           <div className="landing-banner">
             <img src="petconnectlanding.png" alt="French Bulldog" />
           </div>
       
-          {/* Boxed images section */}
           <div className="function-landing">
             <div className="image-box">
-              <img src="Login.png" alt="Login" />
+            <Link to="/login">
+                <img src="Login.png" alt="Login" />
+            </Link>
             </div>
-            <div className="image-box">
-              <img src="Login.png" alt="Login" />
-            </div>
-            <div className="image-box">
-              <img src="Login.png" alt="Login" />
-            </div>
-            <div className="image-box">
-              <img src="Login.png" alt="Login" />
-            </div>
-          </div>
+        </div>
         </div>
       );
     }
