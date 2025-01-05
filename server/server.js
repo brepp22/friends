50a978ff817bd4ac660ab7efdeec1bb67938ee83
brepp22 <brepp@hotmail.com>
2025-01-05 10:58:44 -0800

    serverless-http added to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const serverless = require('serverless-http');
 const app = express()
 const cors = require('cors')
 app.use(express.json())
@@ -14,3 +15,5 @@ app.use('/api', petRouter)
 app.listen(9000 , () => {
     console.log("server started on port 9000")
 })
+
+module.exports.handler = serverless(app)
