25211318f1e6a4cf57bf982940ccde3c5f961625
brepp22 <brepp@hotmail.com>
2025-01-06 15:49:46 -0800

    db-config and knex updated

--- server/api/config/knexfile.js
+++ server/api/config/knexfile.js
@@ -1,28 +1,61 @@
 
+// const sharedConfig = {
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   migrations: {
+//     directory: './data/migrations',
+//   },
+//   seeds: {
+//     directory: './data/seeds',
+//   },
+  
+//   pool: {
+//     afterCreate: (conn, done) => {
+//       conn.run('PRAGMA foreign_keys = ON', done)
+//     },
+//   },
+// }
+
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: { filename: './data/friends.db3' },
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: { filename: './data/testing.db3' },
+//   },
+// }
+
+const path = require('path');
+
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
   migrations: {
-    directory: './data/migrations',
+    directory: path.join(__dirname, '../data/migrations'),
   },
   seeds: {
-    directory: './data/seeds',
+    directory: path.join(__dirname, '../data/seeds'),
   },
-  
   pool: {
     afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done)
+      conn.run('PRAGMA foreign_keys = ON', done);
     },
   },
-}
+};
 
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: { filename: './data/friends.db3' },
+    connection: { filename: path.join(__dirname, '../data/friends.db3') },
   },
   testing: {
     ...sharedConfig,
-    connection: { filename: './data/testing.db3' },
+    connection: { filename: path.join(__dirname, '../data/testing.db3') },
   },
-}
+  production: {
+    ...sharedConfig,
+    connection: { filename: path.join(__dirname, '../data/friends.db3') },
+  },
+};
