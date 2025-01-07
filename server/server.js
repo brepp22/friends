cccc5c360aa9844c2f2c90b652695bc209b19905
brepp22 <brepp@hotmail.com>
2025-01-06 17:52:42 -0800

    Fix start script

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const serverless = require('serverless-http');
 const app = express()
 const cors = require('cors')
 app.use(express.json())
@@ -12,11 +11,11 @@ app.use('/api' , userRouter)
 app.use('/api', petRouter)
 
 
-app.listen(9000 , () => {
+app.listen(process.env.PORT || 9000 , () => {
     console.log("server started on port 9000")
 })
 
-module.exports.handler = serverless(app)
+
 
 // const express = require('express');
 // const serverless = require('serverless-http');
