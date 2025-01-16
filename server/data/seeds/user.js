3fe305590cd956ec6cf9a6680ab38f0bcdc5f4cd
brepp22 <brepp@hotmail.com>
2025-01-15 17:43:15 -0800

    knex , seeds and migrations ran for production

--- server/data/seeds/user.js
+++ server/data/seeds/user.js
@@ -1,17 +1,55 @@
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> } 
+//  */
+
+// const bcrypt = require('bcryptjs')
+
+// exports.seed = async function(knex) {
+//   // Deletes ALL existing entries
+//   await knex('users').truncate()
+//   await knex('users').insert([
+//     { email: 'ellepetshop@gmail.com', username: 'Elleanor', password: bcrypt.hashSync('hotfudge22' , 8)},
+//     { email: 'k.money@hotmail.com', username: 'Kirby' , password: bcrypt.hashSync('hello1234', 8)},
+//     { email: 'mel@prodigy.net', username: 'Mel' , password: bcrypt.hashSync ('kirby04', 8)},
+//     { email: 'test@gmail.com', username: 'test' , password: bcrypt.hashSync('helloworld01', 8)},
+//   ]);
+// };
+
+
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('users').truncate()
+exports.seed = async function (knex) {
+  // Clear existing entries
+  await knex('users').del();
+
+  // Insert seed data
   await knex('users').insert([
-    { email: 'ellepetshop@gmail.com', username: 'Elleanor', password: bcrypt.hashSync('hotfudge22' , 8)},
-    { email: 'k.money@hotmail.com', username: 'Kirby' , password: bcrypt.hashSync('hello1234', 8)},
-    { email: 'mel@prodigy.net', username: 'Mel' , password: bcrypt.hashSync ('kirby04', 8)},
-    { email: 'test@gmail.com', username: 'test' , password: bcrypt.hashSync('helloworld01', 8)},
+    {
+      email: 'ellepetshop@gmail.com',
+      username: 'Elleanor',
+      password: bcrypt.hashSync('hotfudge22', 8),
+    },
+    {
+      email: 'k.money@hotmail.com',
+      username: 'Kirby',
+      password: bcrypt.hashSync('hello1234', 8),
+    },
+    {
+      email: 'mel@prodigy.net',
+      username: 'Mel',
+      password: bcrypt.hashSync('kirby04', 8),
+    },
+    {
+      email: 'test@gmail.com',
+      username: 'test',
+      password: bcrypt.hashSync('helloworld01', 8),
+    },
   ]);
 };
+
